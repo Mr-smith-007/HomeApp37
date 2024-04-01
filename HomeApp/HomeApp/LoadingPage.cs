5091d3d9637350a37473c463cf50aec501a74af6
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-04-01 09:56:54 +0300

    Добавьте файлы проекта.

---
+++ HomeApp/HomeApp/LoadingPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HomeApp
+{
+    public class LoadingPage : ContentPage
+    {
+        public LoadingPage()
+        {
+            Label header = new Label() { Text = $"Запуск вашего первого приложения{Environment.NewLine} на Xamarin..." };
+                        
+            header.Opacity = 0;
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+            header.FontSize = 21;
+            
+            header.FadeTo(1, 3000);
+
+            
+            this.Content = header;
+        }
+    }
+}
