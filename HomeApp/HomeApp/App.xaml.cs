3ceb25e30f5fb1edbfde43ad71cb4f1c6cf2853a
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-04-04 16:05:39 +0300

    ScrollLayout

--- HomeApp/HomeApp/App.xaml.cs
+++ HomeApp/HomeApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HomeApp
         {
             InitializeComponent();
 
-            MainPage = /*new RoomsPage();*/new LoginPage();//new LoadingPage();//new MainPage();
+            MainPage = new DevicesPage(); //new RoomsPage(); //new LoginPage(); //new LoadingPage(); //new MainPage();
         }
 
         protected override void OnStart()
