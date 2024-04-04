791bf2d4313a76ec6325979da607f166957a7e81
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-04-04 10:54:25 +0300

    Extentions

---
+++ HomeApp/HomeApp/ButtonViewExtention.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.PlatformConfiguration.TizenSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace HomeApp
+{
+    public class ButtonViewExtention : IMarkupExtension
+    {
+        public ButtonStyle ButtonStyle { get; set; }
+                
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            switch (ButtonStyle)
+            {
+                case ButtonStyle.Default:
+                    return 0;
+                case ButtonStyle.SemiRound:
+                    return 15;
+                case ButtonStyle.Round:
+                    return 30;
+                default:
+                    return 0;
+            }
+        }
+    }
+
+    public enum ButtonStyle
+    {
+        Default,
+        SemiRound,
+        Round
+    }
+}
