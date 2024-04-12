566f5b7be7b53a8747b17c147ed0b127cff3e180
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-04-12 16:48:07 +0300

    CustomRenderer

---
+++ HomeApp/HomeApp.Android/Renderers/CustomEditorRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HomeApp.Droid.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+namespace HomeApp.Droid.Renderers
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
